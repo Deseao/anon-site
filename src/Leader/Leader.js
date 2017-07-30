78db3e74fbad083a3713fa4e1e704155fb798e25
Alex Haynes <ahaynes@vailsys.com>
2017-07-30 14:47:18 -0500

    Minor UI change and merge of the leader route

--- src/Leader/Leader.js
+++ src/Leader/Leader.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import CreateGroup from './CreateGroup'
+import SendMessage from './SendMessage'
 const Leader = () => {
   return (
     <div>
-      <Link to="/leader/createGroup">Create New Group</Link>
-      <Link to="/leader/sendMessage">Send a Message to an Existing Group</Link>
+      <CreateGroup />
+      <br />
+      <SendMessage/>
     </div>
   )
 }
